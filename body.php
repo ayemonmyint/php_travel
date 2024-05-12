6e41b7fff5250f3da2088b385e22391e490f521b
ayemonmyint <ayemonmyint88@gmail.com>
2024-05-12 12:08:58 +0630

    intro_page_body_part

---
+++ body.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .card-img-top {
+            width: 100%;
+            height: 80%;
+        }
+
+        .img-container {
+            position: relative;
+            overflow: hidden;
+        }
+
+        .img-container img {
+            transition: transform 0.3s ease;
+        }
+
+        .img-container:hover img {
+            transform: scale(1.1);
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="text-center fw-bold">BEST OFFERS</h2>
+        <h6 class="text-center fw-light">CHECK OUT OUR TOP-RATED TOURS</h6>
+        <div class="d-flex justify-content-evenly mt-5">
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/bagan.jpg" class="card-img-top " alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Bagan</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/inle.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Inle</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/kalaw.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Kalaw</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/mandalay.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Mandalay</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-evenly mt-5">
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/mon.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Mon</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/pathein.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Pathein</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/Taunggyi.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Taunggyi</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+            <div class="card img-container" style="width: 18rem;">
+                <img src="img/yangon.jpg" class="card-img-top" alt="animal">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text ">Yangon</p>
+                    <p class="card-text ">$5000</p>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
